340e30d2d3c39b865ab5332fd4b887ed6bd8aa28
Leo Chen <chenl3@rose-hulman.edu>
2021-09-27 16:10:47 -0400

    UPDATE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    // console.log(e.target.new_task_description.value);
+    handleToDo(e.target.querySelector("#new-task-description").value);
+  })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p');
+  p.textContent = todo;
+  document.querySelector('#list').appendChild(p);
+}
+
+
